82e5808960194e7a78e9746fea7af3547e1fb330
Gerber Rogelio Díaz Santay <rogerdisan18@gmail.com>
2026-03-24 23:58:08 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+int totalEvaluados = 0;
+int totalPublicados = 0;
+int totalRechazados = 0;
+int totalEnRevision = 0;
+int totalPublicadosConAjustes = 0;
+int impactoAltoCount = 0;
+int impactoMedioCount = 0;
+int impactoBajoCount = 0;
+
